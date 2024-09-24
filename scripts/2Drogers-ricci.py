8f126bc1ee7cde9f81c1e278f6b8d3ff957a5844
Owen Parry <owen.parry@ukaea.uk>
2024-09-24 13:28:55 +0100

    JB changes

--- scripts/2Drogers-ricci.py
+++ scripts/2Drogers-ricci.py
@@ -1,9 +1,15 @@
 from firedrake import (
     Constant,
+    derivative,
+    DirichletBC,
     dx,
     exp,
     Function,
     FunctionSpace,
+    grad,
+    inner,
+    NonlinearVariationalProblem,
+    NonlinearVariationalSolver,
     PETSc,
     SpatialCoordinate,
     split,
@@ -13,8 +19,6 @@ from firedrake import (
 )
 
 from common import (
-    nl_solve_setup,
-    phi_solve_setup,
     poisson_bracket,
     read_rr_config,
     rr_DG_upwind_term,
@@ -23,16 +27,47 @@ from common import (
     rr_steady_state,
     set_up_mesh,
 )
-from irksome import Dt
 import os.path
 from pyop2.mpi import COMM_WORLD
 import time
+
+
+def setup_nl_solver(eqn, U1, Jp, bcs):
+    nl_prob = NonlinearVariationalProblem(eqn, U1, Jp=Jp, bcs=bcs)
+    nl_params = {
+        # "snes_monitor": None,
+        # "snes_converged_reason": None,
+        "ksp_type": "gmres",
+        # "ksp_converged_reason": None,
+        # "snes_atol": 1.0e-50,
+        # "snes_stol": 1.0e-50,
+        # "snes_rtol": 1.0e-7,
+        # "ksp_atol": 1.0e-50,
+        # "ksp_rtol": 1.0e-9,
+        # "ksp_max_it": 50,
+        "pc_type": "fieldsplit",
+        "pc_fieldsplit_type": "additive",
+        "pc_fieldsplit_0_fields": "0,1,2",
+        "pc_fieldsplit_1_fields": "3",
+        "fieldsplit_0_ksp_type": "preonly",
+        "fieldsplit_0_pc_type": "bjacobi",
+        "fieldsplit_0_sub_pc_type": "ilu",
+        "fieldsplit_1_ksp_type": "preonly",
+        "fieldsplit_1_ksp_reuse_preconditioner": None,
+        "fieldsplit_1_pc_type": "lu",
+        "fieldsplit_1_pc_factor_mat_solver_type": "mumps",
+    }
+    return NonlinearVariationalSolver(nl_prob, solver_parameters=nl_params)
 
 
 def exp_T_term(T, phi, cfg, eps=1e-2):
     e = Constant(cfg["normalised"]["e"])
     Lambda = Constant(cfg["physical"]["Lambda"])
     return exp(Lambda - e * phi / sqrt(T * T + eps * eps))
+
+
+def lhs_term(start, end, test):
+    return inner(end - start, test) * dx
 
 
 def rogers_ricci2D():
@@ -51,31 +86,35 @@ def rogers_ricci2D():
     T_space = FunctionSpace(mesh, DG_or_CG, cfg["numerics"]["fe_order"]["T"])  # T
     phi_space = FunctionSpace(mesh, "CG", cfg["numerics"]["fe_order"]["phi"])  # phi
 
-    # Functions (combine time-evolved function spaces to facilitate interaction with Irksome)
-    phi = Function(phi_space)
-    phi.rename("potential")
-    combined_space = n_space * w_space * T_space
-    time_evo_funcs = Function(combined_space)
-    n, w, T = split(time_evo_funcs)
-    subspace_indices = dict(n=0, w=1, T=2)
+    combined_space = n_space * w_space * T_space * phi_space
+    state0 = Function(combined_space)
+    state1 = Function(combined_space)
+    n0, w0, T0, phi0 = split(state0)
+    n1, w1, T1, phi1 = split(state1)
+    subspace_indices = dict(n=0, w=1, T=2, phi=3)
+
+    n_avg = (n0 + n1) / 2
+    w_avg = (w0 + w1) / 2
+    T_avg = (T0 + T1) / 2
+    phi_avg = phi1
 
     # Rename fields and set up funcs for output
-    subspace_names = dict(
-        n="density",
-        w="vorticity",
-        T="temperature",
-    )
+    subspace_names = dict(n="density", w="vorticity", T="temperature", phi="potential")
     for fld in subspace_indices.keys():
-        time_evo_funcs.sub(subspace_indices[fld]).rename(subspace_names[fld])
+        state0.sub(subspace_indices[fld]).rename(subspace_names[fld])
+
+    # Time step
+    time_cfg = cfg["time"]
+    t = Constant(time_cfg["t_start"])
+    t_end = time_cfg["t_end"]
+    dt = Constant(time_cfg["t_end"] / time_cfg["num_steps"])
 
     # Source functions
     n_src = rr_src_term(n_space, x, y, "n", cfg)
     T_src = rr_src_term(T_space, x, y, "T", cfg)
 
-    phi_solver = phi_solve_setup(phi_space, phi, w, cfg)
-
     # Assemble variational problem
-    n_test, w_test, T_test = TestFunctions(combined_space)
+    n_test, w_test, T_test, phi_test = TestFunctions(combined_space)
 
     sigma_cs_over_R = Constant(
         cfg["normalised"]["sigma"] * cfg["normalised"]["c_s0"] / cfg["normalised"]["R"]
@@ -85,61 +124,66 @@ def rogers_ricci2D():
     isDG = cfg["numerics"]["discretisation"] == "DG"
 
     n_terms = (
-        (
-            Dt(n)
-            - one_over_B * poisson_bracket(phi, n)
-            + sigma_cs_over_R * n * exp_T_term(T, phi, cfg)
+        lhs_term(n0, n1, n_test)
+        + dt
+        * (
+            -one_over_B * poisson_bracket(phi_avg, n_avg)
+            + sigma_cs_over_R * n_avg * exp_T_term(T_avg, phi_avg, cfg)
             - n_src
         )
         * n_test
         * dx
     )
     if isDG:
-        n_terms += rr_DG_upwind_term(n, n_test, phi, mesh, cfg)
+        n_terms += dt * rr_DG_upwind_term(n_avg, n_test, phi_avg, mesh, cfg)
     elif cfg["numerics"]["do_streamline_upwinding"]:
-        n_terms += rr_SU_term(n, n_test, phi, h_SU, cfg)
+        n_terms += dt * rr_SU_term(n_avg, n_test, phi_avg, h_SU, cfg)
 
     e = cfg["normalised"]["e"]
     m_i = cfg["normalised"]["m_i"]
     Omega_ci = cfg["normalised"]["omega_ci"]
-    w_terms = (
+    w_terms = lhs_term(w0, w1, w_test) + dt * (
         (
-            Dt(w)
-            - one_over_B * poisson_bracket(phi, w)
+            -one_over_B * poisson_bracket(phi_avg, w_avg)
             - Constant(sigma_cs_over_R * m_i * Omega_ci * Omega_ci / e)
-            * (1 - exp_T_term(T, phi, cfg))
+            * (1 - exp_T_term(T_avg, phi_avg, cfg))
         )
         * w_test
         * dx
     )
     if isDG:
-        w_terms += rr_DG_upwind_term(w, w_test, phi, mesh, cfg)
+        w_terms += dt * rr_DG_upwind_term(w_avg, w_test, phi_avg, mesh, cfg)
     elif cfg["numerics"]["do_streamline_upwinding"]:
-        w_terms += rr_SU_term(w, w_test, phi, h_SU, cfg)
+        w_terms += dt * rr_SU_term(w_avg, w_test, phi_avg, h_SU, cfg)
 
     T_terms = (
-        (
-            Dt(T)
-            - one_over_B * poisson_bracket(phi, T)
+        lhs_term(T0, T1, T_test)
+        + dt
+        * (
+            -one_over_B * poisson_bracket(phi_avg, T_avg)
             + Constant(sigma_cs_over_R * 2 / 3)
-            * T
-            * (1.71 * exp_T_term(T, phi, cfg) - 0.71)
+            * T_avg
+            * (1.71 * exp_T_term(T_avg, phi_avg, cfg) - 0.71)
             - T_src
         )
         * T_test
         * dx
     )
     if isDG:
-        T_terms += rr_DG_upwind_term(T, T_test, phi, mesh, cfg)
+        T_terms += dt * rr_DG_upwind_term(T_avg, T_test, phi_avg, mesh, cfg)
     elif cfg["numerics"]["do_streamline_upwinding"]:
-        T_terms += rr_SU_term(T, T_test, phi, h_SU, cfg)
+        T_terms += dt * rr_SU_term(T_avg, T_test, phi_avg, h_SU, cfg)
 
-    F = n_terms + w_terms + T_terms
+    phi_terms = (
+        grad(phi_avg)[0] * grad(phi_test)[0] + grad(phi_avg)[1] * grad(phi_test)[1]
+    ) * dx + w_avg * phi_test * dx
 
-    time_cfg = cfg["time"]
-    t = Constant(time_cfg["t_start"])
-    t_end = time_cfg["t_end"]
-    dt = Constant(time_cfg["t_end"] / time_cfg["num_steps"])
+    F = n_terms + w_terms + T_terms + phi_terms
+    F_for_jacobian = F + phi_avg * phi_test * dx
+    Jp = derivative(F_for_jacobian, state1)
+
+    bcs = DirichletBC(combined_space.sub(subspace_indices["phi"]), 0.0, "on_boundary")
+    nl_solver = setup_nl_solver(F, state1, Jp, bcs)
 
     # Set ICs
     if cfg["model"]["start_from_steady_state"]:
@@ -149,11 +193,9 @@ def rogers_ricci2D():
         T_init = cfg["normalised"]["T_init"]
         w_init = 0.0
 
-    time_evo_funcs.sub(subspace_indices["n"]).interpolate(n_init)
-    time_evo_funcs.sub(subspace_indices["T"]).interpolate(T_init)
-    time_evo_funcs.sub(subspace_indices["w"]).interpolate(w_init)
-
-    stepper = nl_solve_setup(F, t, dt, time_evo_funcs, cfg)
+    state0.sub(subspace_indices["n"]).interpolate(n_init)
+    state0.sub(subspace_indices["T"]).interpolate(T_init)
+    state0.sub(subspace_indices["w"]).interpolate(w_init)
 
     # Set up output
     outfile = VTKFile(os.path.join(cfg["root_dir"], cfg["output_base"] + ".pvd"))
@@ -161,24 +203,26 @@ def rogers_ricci2D():
     PETSc.Sys.Print("\nTimestep loop:")
     step = 0
 
+    state1.assign(state0)
+
     twall_last_info = time.time()
     while float(t) < float(t_end):
         if (float(t) + float(dt)) > t_end:
             dt.assign(t_end - float(t))
             PETSc.Sys.Print(f"  Last dt = {dt}")
-        phi_solver.solve()
+        t.assign(float(t) + float(dt))
+        nl_solver.solve()
+        state0.assign(state1)
 
         # Write fields on output steps
         if step % cfg["time"]["output_freq"] == 0:
             outfile.write(
-                time_evo_funcs.sub(subspace_indices["n"]),
-                time_evo_funcs.sub(subspace_indices["w"]),
-                time_evo_funcs.sub(subspace_indices["T"]),
-                phi,
+                state0.sub(subspace_indices["n"]),
+                state0.sub(subspace_indices["w"]),
+                state0.sub(subspace_indices["T"]),
+                state0.sub(subspace_indices["phi"]),
             )
 
-        stepper.advance()
-        t.assign(float(t) + float(dt))
         if step % cfg["time"]["info_freq"] == 0:
             dtwall_last_info = time.time() - twall_last_info
             twall_last_info = time.time()
